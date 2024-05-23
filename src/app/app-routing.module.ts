af1786801d449748f6973a7619274043eaffdd9c
Lucas <silvasousalucas@live.com>
2024-05-23 17:39:57 -0300

    created the front-end for the login and register page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,22 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'recover-password',
+    loadChildren: () => import('./pages/recoverPassword/recover-password/recover-password.module').then( m => m.RecoverPasswordPageModule)
+  },
+  {
+    path: 'recover-password',
+    loadChildren: () => import('./pages/recoverPassword/recover-password/recover-password.module').then( m => m.RecoverPasswordPageModule)
   }
 ];
 @NgModule({
